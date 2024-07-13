11edde2cf2eb1e17e1bb5bd04cff40048aac40ab
alisson <estudossbh@gmail.com>
2024-07-12 23:44:40 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// spa
+// single page aplication
+const Button: React.FC<{ change: boolean; color: string; text: string; onClick: () => void }> = (props) => {
+  return <button type='button' style={{ backgroundColor: props.color }} onClick={props.onClick}> 
+    {props.change ? 'mudou' : props.text}
+  </button>;
+};
+
+const App = () => {
+  const [change, setChange] = React.useState(false);
+
+  const handleClick = () => {
+    setChange(state => !state);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit aaaaa <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +33,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button change={change} color='red' text='teste' onClick={handleClick} />
     </div>
   );
 }
